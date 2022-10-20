360ea90d8f8004f3292843728684180440792e89
mmdchnar <mmdchnar@gmail.com>
2022-10-20 16:29:37 +0330

    fix '=' in regext

--- bot.py
+++ bot.py
@@ -223,7 +223,7 @@ def main():
                     # Write servers to file
                     for channel in channels:
                         for message in bot.iter_messages(channel, limit=30):
-                            servers = findall(r'(vmess://[a-z0-9A-Z_]+)(\s|\n)?',message.message)
+                            servers = findall(r'(vmess://[a-z0-9A-Z=]+)(\s|\n)?',message.message)
 
                             for server in servers:
                                 file.write(server[0])
